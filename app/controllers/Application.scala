47f2b56b3b98a136df1f93fad1f97d2ebd8c2680
Chris Fusting <cfusting@gmail.com>
2016-05-24 16:08:38 -0400

    Bot will talk back!

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,19 @@
 package controllers
 
+import javax.inject.Inject
+
 import akka.util.ByteString
 import play.api._
 import play.api.http.HttpEntity
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 
-class Application extends Controller {
+import scala.concurrent.Future
+
+class Application @Inject() (ws: WSClient) extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -16,6 +24,7 @@ class Application extends Controller {
       val challenge = request.getQueryString("hub.challenge") map { _.trim } getOrElse("")
       val verify = request.getQueryString("hub.verify_token") map { _.trim } getOrElse("")
       if(verify == "penguin_verify") {
+        Logger.info("Penguin verified.")
         Result(
           header = ResponseHeader(200, Map.empty),
           body = HttpEntity.Strict(ByteString(challenge), Some("text/plain"))
@@ -25,10 +34,55 @@ class Application extends Controller {
       }
   }
 
-  def webhookPost = Action(parse.json) {
+//  case class FMessage(obj: String, entry: Seq[Entry])
+//  case class FMessage(obj: String)
+//  case class Entry(id: Double, time: Double, messaging: Seq[Messaging])
+//  case class Messaging(sender: Double, recipient: Double, message: Message, timestamp: Double)
+//  case class Sender(id: Double)
+//  case class Recipient(id: Double)
+//  case class Message(mid: String, seq: Int, text: String)
+
+//  implicit val fMessagereads: Reads[FMessage] = (
+//    (JsPath \ "object").read[String] and
+//      (JsPath \ "entry").read[Seq[Entry]]
+//    )(FMessage.apply _)
+
+//  implicit val entryReads: Reads[Entry] = (
+//    (JsPath \ "id").read[Double] and
+//      (JsPath \ "time").read[Double] and
+//      (JsPath \ "messaging").read[Seq[Messaging]]
+//    )(Entry.apply _)
+//
+//  implicit val messagingReads: Reads[Messaging] = (
+//    (JsPath \ "sender" \ "id").read[Double] and
+//      (JsPath \ "recipient" \ "id").read[Double] and
+//      (JsPath \ "message").read[Message] and
+//      (JsPath \ "timestamp").read[Double]
+//    )(Messaging.apply _)
+
+//  implicit val senderReads: Reads[Sender] = (JsPath \ "id").read[Sender]
+
+//  implicit val recipientReads: Reads[Recipient] = (JsPath \ "id").read[Recipient]
+
+//  implicit val messageReads: Reads[Message] = (
+//    (JsPath \ "mid").read[String] and
+//      (JsPath \ "seq").read[Int] and
+//      (JsPath \ "text").read[String]
+//    )(Message.apply _)
+
+  def webhookPost = Action(BodyParsers.parse.json) {
     implicit request =>
-    Logger.error("request: " + request.body)
-    Ok("cats")
+      Logger.info("request: " + request.body)
+      val json: JsValue = request.body
+      val result = (((json \ "entry")(0).get \ "messaging")(0).get \ "sender" \ "id").get
+      val dat = Json.obj(
+        "recipient" -> Json.obj("id" -> result),
+        "message" -> Json.obj("text" -> "I know wassup!")
+      )
+      val res: Future[WSResponse] = ws.url("https://graph.facebook.com/v2.6/me/messages")
+        .withQueryString("access_token" -> "EAAWOb6Mv7N4BAMtRoYHBaLBMZCyZAGDWmwFZCKr1KarrJUy3ZAHmkGd1OVAjUBdJzAwbYZAyiZCFYJqjWZBLyjvJILTjXhLz95q5lGdAe1NWUcKWSixKeCNumHEAZBZAkh1EWNOjOYiiGv1jiUZCttghM0ZCF4Ppv79MVZBjibuwmFQ6wQZDZD")
+        .post(dat)
+      Ok("")
   }
 
 }
